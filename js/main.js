f8955671b7bd14a12d02007dace62f1040d5e2ec
Daniel Lauzon <daniel.lauzon@gmail.com>
2012-09-14 23:03:51 -0400

    portfolio sidebar

--- js/main.js
+++ js/main.js
@@ -1,16 +1,27 @@
 
 (function(){  // the entire file is scope inside a anonymous function execution.
-	
-	$(document).ready(function(){
-	    setupOrientationHandler();
-	});
+
+    $(document).ready(function(){
+        setupOrientationHandler();
+
+    // hidden/commented in portfolio
+    $('.carousel').carousel(/* {interval:1000}*/)
+    
+    // this is for affix - later
+    if(0)$('.dl-sidenav').affix({
+        offset: {
+            top: function () { return $(window).width() <= 980 ? 290 : 210 }
+            , bottom: 270
+        }
+    })
+});
 
     function setupOrientationHandler(){
         function orientationChange(){
-			MBP.scaleFix();
+            MBP.scaleFix();
             MBP.hideUrlBar();
         }
-        
+
         // bind to event
         $(window).bind('orientationchange', orientationChange);
         // invoke on Setup.
