81c0eeb1625a36fbcc6a14391f70c8ee9a7eb10e
Daniel Lauzon <daniel.lauzon@gmail.com>
2012-09-16 17:14:27 -0400

    Adjust active navbar title - yuck

--- js/main.js
+++ js/main.js
@@ -4,17 +4,20 @@
     $(document).ready(function(){
         setupOrientationHandler();
 
-    // hidden/commented in portfolio
-    $('.carousel').carousel(/* {interval:1000}*/)
-    
-    // this is for affix - later
-    if(0)$('.dl-sidenav').affix({
-        offset: {
-            top: function () { return $(window).width() <= 980 ? 290 : 210 }
-            , bottom: 270
+        // hidden/commented in portfolio
+        $('.carousel').carousel(/* {interval:1000}*/)
+        
+        // Adjust active navbar title - yuck
+        var $t = $('title').html();        
+        if ($t.match(/Portfolio/)){
+            $('ul.nav li').removeClass('active');
+            $('ul.nav li a[href="/portfolio"]').parent().addClass('active');
         }
-    })
-});
+        if ($t.match(/Home/)){
+            $('ul.nav li').removeClass('active');
+            $('ul.nav li a[href="/home"]').parent().addClass('active');
+        }
+    });
 
     function setupOrientationHandler(){
         function orientationChange(){
