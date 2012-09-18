0c731f12becba5999f34c7e1d298c8448b21015a
Daniel Lauzon <daniel.lauzon@gmail.com>
2012-09-18 12:30:49 -0400

    navbar dynamic

--- js/main.js
+++ js/main.js
@@ -8,8 +8,10 @@
         $('.carousel').carousel(/* {interval:1000}*/)
         
         // Adjust active navbar title - yuck
-        var $t = $('title').html();        
+        var $t = $('title').html();  
+        console.log('match',$t);
         if ($t.match(/Portfolio/)){
+            console.log('found portfolio',$t);
             $('ul.nav li').removeClass('active');
             $('ul.nav li a[href="/portfolio"]').parent().addClass('active');
         }
